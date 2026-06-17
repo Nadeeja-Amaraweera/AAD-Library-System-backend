95cde63f82978581ef98c95e24ecd1d8932e2998
Nadeeja Amaraweera <nadeejaamaraweera@gmail.com>
2026-06-17 21:21:41 +0530

    Add BookController and refactor BookServiceImpl

---
+++ src/main/java/lk/ijse/AADLibrarySystem/Controller/BookController.java
@@ -0,0 +1,11 @@
+package lk.ijse.AADLibrarySystem.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("v1/api/book")
+public class BookController {
+}
--- src/main/java/lk/ijse/AADLibrarySystem/Service/Impl/BookServiceImpl.java
+++ src/main/java/lk/ijse/AADLibrarySystem/Service/Impl/BookServiceImpl.java
@@ -117,7 +117,4 @@ public class BookServiceImpl implements BookService {
         }
     }
 
-
-
-
 }
