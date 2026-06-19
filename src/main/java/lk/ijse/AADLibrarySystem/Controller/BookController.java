e487c900df65baa42c274437a58fd7375fbf4b7a
Nadeeja Amaraweera <nadeejaamaraweera@gmail.com>
2026-06-19 18:41:46 +0530

    Add saveBook method to BookController for saving book details

--- src/main/java/lk/ijse/AADLibrarySystem/Controller/BookController.java
+++ src/main/java/lk/ijse/AADLibrarySystem/Controller/BookController.java
@@ -1,11 +1,32 @@
 package lk.ijse.AADLibrarySystem.Controller;
 
+import lk.ijse.AADLibrarySystem.Constant.CommonResponse;
+import lk.ijse.AADLibrarySystem.DTO.BookDTO;
+import lk.ijse.AADLibrarySystem.Service.BookService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static lk.ijse.AADLibrarySystem.Constant.ResponseMessage.SUCCESS_MESSAGE;
+import static lk.ijse.AADLibrarySystem.Constant.ResponseStatusCode.OPERATION_SUCCESS;
 
 @Slf4j
 @RestController
 @RequestMapping("v1/api/book")
 public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping(value = "/save",produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse saveBook(@RequestBody BookDTO bookDTO){
+        BookDTO bookDTO1 = bookService.saveBook(bookDTO);
+        return new CommonResponse(OPERATION_SUCCESS, bookDTO1, SUCCESS_MESSAGE);
+    }
 }
