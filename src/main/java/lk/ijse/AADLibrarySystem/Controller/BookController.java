97b3eb92a87bc648099199af54fac145536f2fd2
Nadeeja Amaraweera <nadeejaamaraweera@gmail.com>
2026-06-20 21:43:41 +0530

    Add updateBook method to BookController for updating book details

--- src/main/java/lk/ijse/AADLibrarySystem/Controller/BookController.java
+++ src/main/java/lk/ijse/AADLibrarySystem/Controller/BookController.java
@@ -5,10 +5,7 @@ import lk.ijse.AADLibrarySystem.DTO.BookDTO;
 import lk.ijse.AADLibrarySystem.Service.BookService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static lk.ijse.AADLibrarySystem.Constant.ResponseMessage.SUCCESS_MESSAGE;
 import static lk.ijse.AADLibrarySystem.Constant.ResponseStatusCode.OPERATION_SUCCESS;
@@ -27,6 +24,12 @@ public class BookController {
     @PostMapping(value = "/save",produces = MediaType.APPLICATION_JSON_VALUE)
     public CommonResponse saveBook(@RequestBody BookDTO bookDTO){
         BookDTO bookDTO1 = bookService.saveBook(bookDTO);
+        return new CommonResponse(OPERATION_SUCCESS, bookDTO1, SUCCESS_MESSAGE);
+    }
+
+    @PutMapping(value = "/update",produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse updateBook(@RequestBody BookDTO bookDTO) {
+        BookDTO bookDTO1 = bookService.updateBook(bookDTO);
         return new CommonResponse(OPERATION_SUCCESS, bookDTO1, SUCCESS_MESSAGE);
     }
 }
